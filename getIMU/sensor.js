4cfe1f76221b28e2846d18856ec2dcf6dcaea854
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-03 16:25:27 +0100

    fixed carrage return

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -1,23 +1,24 @@
 function checkSensors(){
-    document.write("This is sensor information available on your device.\n");
+    document.write("This is sensor information available on your device"+'<br>');
 
     if ('Accelerometer' in window) {
         // The `Accelerometer` interface is supported by the browser.
         // Does the device have an accelerometer, though?
-        document.write("Yes, Acc sensor is available.");
+        document.write("Yes, Acc sensor is available."+'<br>');
     }
     else {
-        document.write("No, Acc sensor is not available.");
+        document.write("No, Acc sensor is not available."+'<br>');
     }
 
     if ('Gyroscope' in window) {
-        document.write("Yes, Gyro sensor is avaialble.\n");
+        document.write("Yes, Gyro sensor is avaialble."+'<br>');
     }
     else {
-        document.write("No, Gyro sensor is not available.\n");
+        document.write("No, Gyro sensor is not available."+'<br>');
     }
 
     if (window.DeviceOrientationEvent) {
+        document.write("Yes, DeviceOrientationEvent is possible"+'<br>');
         window.addEventListener("deviceorientation", (event) => {
             const rotateDegrees = event.alpha; // alpha: rotation around z-axis
             const leftToRight = event.gamma; // gamma: left to right
@@ -30,6 +31,6 @@ function checkSensors(){
 
     const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
         // do something amazing
-        document.write("It is moving around!!")
+        document.write("It is moving around!!"+'<br>')
     };
 }
