6fdd573ae47a35e937dd46427a8caf5456136b2f
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-03 14:40:23 +0100

    added availability

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -1,11 +1,26 @@
-const myHeading = document.querySelector("h1");
-if ('Accelerometer' in window) {
-        // The `Accelerometer` interface is supported by the browser.
-        // Does the device have an accelerometer, though?
-    myHeading.textContent = "Hello world!";
-    return true;
-}
-else {
-    myHeading.textContent = "Bye!";
-    return false;
-}
+(function () {
+
+    function getAvailability() {
+        if ('Accelerometer' in window) {
+            // The `Accelerometer` interface is supported by the browser.
+            // Does the device have an accelerometer, though?
+            myHeading.textContent = "Hello world!";
+        return true;
+        }
+        else {
+            myHeading.textContent = "Bye!";
+            return false;
+        }
+    }
+
+    if (window.MobileDevice == undefined) {
+        window.MobileDevice = {};
+    }
+
+    window.MobileDevice.getAvailability = getAvailability;
+})();
+
+
+
+
+
