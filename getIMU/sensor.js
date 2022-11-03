3a23d51c332829c2497d06ab9ea61626877ad9a9
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-03 16:08:48 +0100

    added gyro

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -1,12 +1,19 @@
-function myfunction(){
-    document.write("This is var display test");
+function checkSensors(){
+    document.write("This is sensor information available on your device.\n");
 
     if ('Accelerometer' in window) {
         // The `Accelerometer` interface is supported by the browser.
         // Does the device have an accelerometer, though?
-        document.write("Yes, acc sensor is available.")
+        document.write("Yes, Acc sensor is available.");
     }
     else {
-        document.write("No, acc sensor is not available.")
+        document.write("No, Acc sensor is not available.");
+    }
+
+    if ('Gyroscope' in window) {
+        document.write("Yes, Gyro sensor is avaialble.\n");
+    }
+    else {
+        document.write("No, Gyro sensor is not available.\n");
     }
 }
