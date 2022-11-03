e640e7716a0ac1e51703a1440b3d5955c78bc219
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-03 15:53:58 +0100

    added checking acc sensor

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -1,3 +1,8 @@
 function myfunction(){
+    if ('Accelerometer' in window) {
+        // The `Accelerometer` interface is supported by the browser.
+        // Does the device have an accelerometer, though?
+        document.write("Yes, it is.")
+    }
     document.write("This is button test");
 }
