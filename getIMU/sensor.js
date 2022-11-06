4ad8d2bce46f10e3057fa85dae72ff6b1589ab68
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-06 15:54:49 +0100

    added permission handling

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -17,17 +17,35 @@ function checkSensors(){
         document.write("No, Gyro sensor is not available."+'<br>');
     }
 
-    if (window.DeviceOrientationEvent) {
-        // for debugging message for iOS
-        document.write("Yes, DeviceOrientationEvent is possible"+'<br>');
-        window.addEventListener("deviceorientation", (event) => {
-            const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-            const leftToRight = event.gamma; // gamma: left to right
-            const frontToBack = event.beta; // beta: front back motion
+    if (DeviceMotionEvent.requestPermission) {
+        DeviceMotionEvent.requestPermission()
+        .then(response => {
+            if (response == "granted") {
+                window.addEventListener("deviceorientation", (event) => {
+                    const rotateDegrees = event.alpha;
+                    const leftToRight = event.gamma;
+                    const frontToBack = event.beta;
 
-            handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-        }, true);
+                    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+                }, true);
+            } else {
+                alert('Device orientation permission not granted');
+            }
+        }).catch((err) => {console.log(err)});
+    } else {
+        alert("Device motion permission access method not available");
     }
+//    if (window.DeviceOrientationEvent) {
+//        window.addEventListener("deviceorientation", (event) => {
+//            const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+//            const leftToRight = event.gamma; // gamma: left to right
+//            const frontToBack = event.beta; // beta: front back motion
+//
+//            handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+//        }, true);
+//    } else {
+//        console.log("Sorry, your browser doens't support Device Orientation");
+//    }
 
     const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
         // when event is occured the display current values
