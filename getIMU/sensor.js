328df04a345e53b5d7ad3dd841e73a7d8a2324ad
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-07 15:31:04 +0100

    added rotationrate

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -19,7 +19,8 @@ function checkSensors(){
                         const accGravityY = motionEvent.accelerationIncludingGravity.y;
                         const accGravityZ = motionEvent.accelerationIncludingGravity.z;
                         const accInterval = motionEvent.interval;
-                        handleMotionEvent(accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval);
+                        const accRotationRate = motionEvent.rotationRate;
+                        handleMotionEvent(accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval, accRotationRate);
                     }, true);
                 } else {
                     alert('Device orientation permission not granted');
@@ -42,7 +43,8 @@ function checkSensors(){
             const accGravityY = motionEvent.accelerationIncludingGravity.y;
             const accGravityZ = motionEvent.accelerationIncludingGravity.z;
             const accInterval = motionEvent.interval;
-            handleMotionEvent(accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval);
+            const accRotationRate = motionEvent.rotationRate;
+            handleMotionEvent(accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval, accRotationRate);
         }, true);
     }
 
@@ -51,22 +53,23 @@ function checkSensors(){
         //This way (calling document.write) shows the values without overwriting (it makes many lines)
         //document.write('<p>Alpha: ' + rotateDegrees + '<br>' + 'Beta: ' + frontToBack + '<br>' + 'Gamma: ' + leftToRight + '</p>');
         //So that showing values using document.getElementById
-        var alphaValStr = 'Alpha: ' + rotateDegrees;
-        var gammaValStr = 'Gamma: ' + leftToRight;
-        var betaValStr = 'Beta: ' + frontToBack;
+        var alphaValStr = 'Alpha-yaw(deg): ' + rotateDegrees;
+        var gammaValStr = 'Gamma-roll(deg): ' + leftToRight;
+        var betaValStr = 'Beta-pitch(deg): ' + frontToBack;
         document.getElementById('rotateDegrees_result').innerHTML = alphaValStr;
         document.getElementById('leftToRight_result').innerHTML = gammaValStr;
         document.getElementById('frontToBack_result').innerHTML = betaValStr;
     };
 
-    const handleMotionEvent = (accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval) => {
-        var accXStr = 'X: ' + accX;
-        var accYStr = 'Y: ' + accY;
-        var accZStr = 'Z: ' + accZ;
-        var accGravityXStr = 'Gravity X: ' + accGravityX;
-        var accGravityYStr = 'Gravity Y: ' + accGravityY;
-        var accGravityZStr = 'Gravity Z: ' + accGravityZ;
-        var accIntervalStr = 'Acc Interval: ' + accInterval;
+    const handleMotionEvent = (accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval, accRotationRate) => {
+        var accXStr = 'X(m/s^2): ' + accX;
+        var accYStr = 'Y(m/s^2): ' + accY;
+        var accZStr = 'Z(m/s^2): ' + accZ;
+        var accGravityXStr = 'X with Gravity(m/s^2): ' + accGravityX;
+        var accGravityYStr = 'Y with Gravity(m/s^2): ' + accGravityY;
+        var accGravityZStr = 'Z with Gravity(m/s^2): ' + accGravityZ;
+        var accIntervalStr = 'Acc Interval(ms): ' + accInterval;
+        var accRotationRateStr = 'Acc RotationRate(deg/s): ' + accRotationRate;
 
         document.getElementById('acc_x_result').innerHTML = accXStr;
         document.getElementById('acc_y_result').innerHTML = accYStr;
@@ -75,5 +78,6 @@ function checkSensors(){
         document.getElementById('acc_gravity_y_result').innerHTML = accGravityYStr;
         document.getElementById('acc_gravity_z_result').innerHTML = accGravityZStr;
         document.getElementById('acc_interval_result').innerHTML = accIntervalStr;
+        document.getElementById('acc_rotationrate_result').innerHTML = accRotationRateStr;
     }
 }
