d6cd692fb0dc89503cfd19c45acdcb4038ffdc08
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-07 10:08:47 +0100

    added devicemotion event

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -18,20 +18,27 @@ function checkSensors(){
     //}
 
     if (DeviceMotionEvent.requestPermission) {
+    //if (typeof(DeviceMotionEvent) !== "undefined" && typeof(DeviceMotionEvent.requestPermission) === "function") {
         DeviceMotionEvent.requestPermission()
-        .then(response => {
-            if (response == "granted") {
-                window.addEventListener("deviceorientation", (event) => {
-                    const rotateDegrees = event.alpha;
-                    const leftToRight = event.gamma;
-                    const frontToBack = event.beta;
+            .then(response => {
+                if (response == "granted") {
+                    window.addEventListener("deviceorientation", (event) => {
+                        const rotateDegrees = event.alpha;
+                        const leftToRight = event.gamma;
+                        const frontToBack = event.beta;
 
-                    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-                }, true);
-            } else {
-                alert('Device orientation permission not granted');
-            }
-        }).catch((err) => {console.log(err)});
+                        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+                    }, true);
+                    window.addEventListener("devicemotion", (e) => {
+                        const accX = e.accelerationIncludingGravity.x;
+                        const accY = e.accelerationIncludingGravity.y;
+                        const accZ = e.accelerationIncludingGravity.z;
+                        handleOrientationEvent(accX, accY, accZ);
+                    }, true);
+                } else {
+                    alert('Device orientation permission not granted');
+                }
+            }).catch((err) => {console.log(err)});
     } else {
         alert("Device motion permission access method not available");
     }
@@ -55,11 +62,17 @@ function checkSensors(){
         var alphaValStr = 'Alpha: ' + rotateDegrees;
         var gammaValStr = 'Gamma: ' + leftToRight;
         var betaValStr = 'Beta: ' + frontToBack;
+        var accXStr = 'X: ' + accX;
+        var accYStr = 'Y: ' + accY;
+        var accZStr = 'Z: ' + accZ;
         //document.getElementById('rotateDegrees_result').value = alphaValStr;
         //document.getElementById('leftToRight_result').value = gammaValStr;
         //document.getElementById('frontToBack_result').value = betaValStr;
         document.getElementById('rotateDegrees_result').innerHTML = alphaValStr;
         document.getElementById('leftToRight_result').innerHTML = gammaValStr;
         document.getElementById('frontToBack_result').innerHTML = betaValStr;
+        document.getElementById('acc_x_result').innerHTML = accXStr;
+        document.getElementById('acc_y_result').innerHTML = accYStr;
+        document.getElementById('acc_z_result').innerHTML = accZStr;
     };
 }
