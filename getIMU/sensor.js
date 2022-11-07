5290b1861e92026df3b65d5ce678914108426ece
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-07 10:26:48 +0100

    code tidy and separated motion and orientation event

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -22,18 +22,18 @@ function checkSensors(){
         DeviceMotionEvent.requestPermission()
             .then(response => {
                 if (response == "granted") {
-                    window.addEventListener("deviceorientation", (event) => {
-                        const rotateDegrees = event.alpha;
-                        const leftToRight = event.gamma;
-                        const frontToBack = event.beta;
+                    window.addEventListener("deviceorientation", (orientationEvent) => {
+                        const rotateDegrees = orientationEvent.alpha;
+                        const leftToRight = orientationEvent.gamma;
+                        const frontToBack = orientationEvent.beta;
 
                         handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
                     }, true);
-                    window.addEventListener("devicemotion", (e) => {
-                        const accX = e.accelerationIncludingGravity.x;
-                        const accY = e.accelerationIncludingGravity.y;
-                        const accZ = e.accelerationIncludingGravity.z;
-                        handleOrientationEvent(accX, accY, accZ);
+                    window.addEventListener("devicemotion", (motionEvent) => {
+                        const accX = motionEvent.accelerationIncludingGravity.x;
+                        const accY = motionEvent.accelerationIncludingGravity.y;
+                        const accZ = motionEvent.accelerationIncludingGravity.z;
+                        handleMotionEvent(accX, accY, accZ);
                     }, true);
                 } else {
                     alert('Device orientation permission not granted');
@@ -56,23 +56,25 @@ function checkSensors(){
 
     const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees, accX, accY, accZ) => {
         // when event is occured the display current values
+        //This document.write shows the values without overwrite
         //document.write('<p>Alpha: ' + rotateDegrees + '<br>' + 'Beta: ' + frontToBack + '<br>' + 'Gamma: ' + leftToRight + '</p>');
         //var sensorValStr = '<p>Alpha: ' + rotateDegrees + '<br>' + 'Beta: ' + frontToBack + '<br>' + 'Gamma: ' + leftToRight + '</p>';
         //document.getElementById('results').innerHTML = sensorValStr;
         var alphaValStr = 'Alpha: ' + rotateDegrees;
         var gammaValStr = 'Gamma: ' + leftToRight;
         var betaValStr = 'Beta: ' + frontToBack;
-        var accXStr = 'X: ' + accX;
-        var accYStr = 'Y: ' + accY;
-        var accZStr = 'Z: ' + accZ;
-        //document.getElementById('rotateDegrees_result').value = alphaValStr;
-        //document.getElementById('leftToRight_result').value = gammaValStr;
-        //document.getElementById('frontToBack_result').value = betaValStr;
         document.getElementById('rotateDegrees_result').innerHTML = alphaValStr;
         document.getElementById('leftToRight_result').innerHTML = gammaValStr;
         document.getElementById('frontToBack_result').innerHTML = betaValStr;
+    };
+
+    const handleMotionEvent = (accX, accY, accZ) => {
+        var accXStr = 'X: ' + accX;
+        var accYStr = 'Y: ' + accY;
+        var accZStr = 'Z: ' + accZ;
+
         document.getElementById('acc_x_result').innerHTML = accXStr;
         document.getElementById('acc_y_result').innerHTML = accYStr;
         document.getElementById('acc_z_result').innerHTML = accZStr;
-    };
+    }
 }
