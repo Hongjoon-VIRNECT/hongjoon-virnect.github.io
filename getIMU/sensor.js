29583fa6bef8271407145ca07310b4808d75e9f4
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-08 11:04:26 +0100

    removed gravity sections

--- getIMU/sensor.js
+++ getIMU/sensor.js
@@ -1,9 +1,3 @@
-function getOrientation(){
-    const rotateDegrees = orientationEvent.alpha;
-    const leftToRight = orientationEvent.gamma;
-    const frontToBack = orientationEvent.beta;
-    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-}
 
 function checkSensors(){
     if (DeviceMotionEvent.requestPermission) {
@@ -22,12 +16,8 @@ function checkSensors(){
                         const accX = motionEvent.acceleration.x;
                         const accY = motionEvent.acceleration.y;
                         const accZ = motionEvent.acceleration.z;
-                        const accGravityX = motionEvent.accelerationIncludingGravity.x;
-                        const accGravityY = motionEvent.accelerationIncludingGravity.y;
-                        const accGravityZ = motionEvent.accelerationIncludingGravity.z;
                         const accInterval = motionEvent.interval;
-                        const accRotationRate = motionEvent.rotationRate;
-                        handleMotionEvent(accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval, accRotationRate);
+                        handleMotionEvent(accX, accY, accZ, accInterval);
                     }, true);
                 } else {
                     alert('Device orientation permission not granted');
@@ -37,22 +27,18 @@ function checkSensors(){
         //alert("Device motion permission access method not available");
         //Since requestPermission is not supported chrome android 17, call them directly without permission grant
         window.addEventListener("deviceorientation", (orientationEvent) => {
-            //const rotateDegrees = orientationEvent.alpha;
-            //const leftToRight = orientationEvent.gamma;
-            //const frontToBack = orientationEvent.beta;
-            //handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-            setInterval (getOrientation, 1000);
+            const rotateDegrees = orientationEvent.alpha;
+            const leftToRight = orientationEvent.gamma;
+            const frontToBack = orientationEvent.beta;
+            handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
         }, true);
+
         window.addEventListener("devicemotion", (motionEvent) => {
             const accX = motionEvent.acceleration.x;
             const accY = motionEvent.acceleration.y;
             const accZ = motionEvent.acceleration.z;
-            const accGravityX = motionEvent.accelerationIncludingGravity.x;
-            const accGravityY = motionEvent.accelerationIncludingGravity.y;
-            const accGravityZ = motionEvent.accelerationIncludingGravity.z;
             const accInterval = motionEvent.interval;
-            const accRotationRate = motionEvent.rotationRate;
-            handleMotionEvent(accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval, accRotationRate);
+            handleMotionEvent(accX, accY, accZ, accInterval);
         }, true);
     }
 
@@ -69,23 +55,15 @@ function checkSensors(){
         document.getElementById('frontToBack_result').innerHTML = betaValStr;
     };
 
-    const handleMotionEvent = (accX, accY, accZ, accGravityX, accGravityY, accGravityZ, accInterval, accRotationRate) => {
+    const handleMotionEvent = (accX, accY, accZ, accInterval) => {
         var accXStr = 'X(m/s^2): ' + accX;
         var accYStr = 'Y(m/s^2): ' + accY;
         var accZStr = 'Z(m/s^2): ' + accZ;
-        var accGravityXStr = 'X with Gravity(m/s^2): ' + accGravityX;
-        var accGravityYStr = 'Y with Gravity(m/s^2): ' + accGravityY;
-        var accGravityZStr = 'Z with Gravity(m/s^2): ' + accGravityZ;
         var accIntervalStr = 'Acc Interval(ms): ' + accInterval;
-        var accRotationRateStr = 'Acc RotationRate(deg/s): ' + accRotationRate;
 
         document.getElementById('acc_x_result').innerHTML = accXStr;
         document.getElementById('acc_y_result').innerHTML = accYStr;
         document.getElementById('acc_z_result').innerHTML = accZStr;
-        document.getElementById('acc_gravity_x_result').innerHTML = accGravityXStr;
-        document.getElementById('acc_gravity_y_result').innerHTML = accGravityYStr;
-        document.getElementById('acc_gravity_z_result').innerHTML = accGravityZStr;
         document.getElementById('acc_interval_result').innerHTML = accIntervalStr;
-        document.getElementById('acc_rotationrate_result').innerHTML = accRotationRateStr;
     }
 }
