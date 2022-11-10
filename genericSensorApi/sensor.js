d4aecd7180134a0f4e8731203cfca8b7ac457259
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-10 18:15:53 +0100

    added timestamp

--- genericSensorApi/sensor.js
+++ genericSensorApi/sensor.js
@@ -31,12 +31,15 @@ function startSensors(){
         let accX = accelerometer.x;
         let accY = accelerometer.y;
         let accZ = accelerometer.z;
+        let accTS = accelerometer.timestamp;
         var accXStr = 'Acc X(m/s^2): ' + accX;
         var accYStr = 'Acc Y(m/s^2): ' + accY;
         var accZStr = 'Acc Z(m/s^2): ' + accZ;
+        var accTSStr = 'timestamp: ' + accTS;
         document.getElementById("acc_x_result").innerHTML = accXStr;
         document.getElementById("acc_y_result").innerHTML = accYStr;
         document.getElementById("acc_z_result").innerHTML = accZStr;
+        document.getElementById("acc_timestamp").innerHTML = accTSStr;
     });
     gyroscope.start();
     accelerometer.start();
