aa678b99c50499328652e84cd9467bd18d36ad42
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-10 16:41:41 +0100

    code tidy

--- genericSensorApi/sensor.js
+++ genericSensorApi/sensor.js
@@ -1,5 +1,5 @@
 function checkSensors(){
-    let accelerometer = null;
+    var accelerometer = null;
 //    DeviceMotionEvent.requestPermission().then(response => {
 //        if (response == 'granted') {
 //            alert("DeviceMotionEvent permission granted");
@@ -13,44 +13,43 @@ function checkSensors(){
 //                    alert("Accelerometer permission not granted")
 //                }
 //            }
-            try {
-                accelerometer = new Accelerometer({ frequency: 200 });
-                accelerometer.onerror = (event) => {
-                // Handle runtime errors.
-                  if (event.error.name === 'NotAllowedError') {
-                      alert("Permission to access sensor was denied.");
-                      //console.log('Permission to access sensor was denied.');
-                  } else if (event.error.name === 'NotReadableError') {
-                        alert('Cannot connect to the sensor.');
-                        //console.log('Cannot connect to the sensor.');
-                  }
-                };
-                accelerometer.start();
-                accelerometer.onreading = () => {
-                    //console.log(e);
-                    //document.write(e);
-                    let x = accelerometer.x;
-                    let y = accelerometer.y;
-                    let z = accelerometer.z;
-                    let interval = accelerometer.interval;
-                    document.getElementById("acc_x_result").innerHTML = x;
-                    document.getElementById("acc_y_result").innerHTML = y;
-                    document.getElementById("acc_z_result").innerHTML = z;
-                    document.getElementById("acc_interval_result").innerHTML = interval;
-
-                };
-            } catch (error) {
-                // Handle construction errors.
-                if (error.name === 'SecurityError') {
-                    alert('Sensor construction was blocked by the Permissions Policy.');
-                    //console.log('Sensor construction was blocked by the Permissions Policy.');
-                } else if (error.name === 'ReferenceError') {
-                    alert('Sensor is not supported by the User Agent.');
-                    //console.log('Sensor is not supported by the User Agent.');
-                } else {
-                    throw error;
-                }
-            }
+    try {
+        accelerometer = new Accelerometer({ frequency: 60 });
+        accelerometer.onerror = (event) => {
+        // Handle runtime errors.
+          if (event.error.name === 'NotAllowedError') {
+              alert("Permission to access sensor was denied.");
+              //console.log('Permission to access sensor was denied.');
+          } else if (event.error.name === 'NotReadableError') {
+                alert('Cannot connect to the sensor.');
+                //console.log('Cannot connect to the sensor.');
+          }
+        };
+        accelerometer.start();
+        accelerometer.onreading = () => {
+            //console.log(e);
+            //document.write(e);
+            let x = Number(accelerometer.x.toFixed(5));
+            let y = Number(accelerometer.y.toFixed(5));
+            let z = Number(accelerometer.z.toFixed(5));
+            let interval = Number(accelerometer.interval.toFixed(5));
+            document.getElementById("acc_x_result").innerHTML = x;
+            document.getElementById("acc_y_result").innerHTML = y;
+            document.getElementById("acc_z_result").innerHTML = z;
+            document.getElementById("acc_interval_result").innerHTML = interval;
+        };
+    } catch (error) {
+        // Handle construction errors.
+        if (error.name === 'SecurityError') {
+            alert('Sensor construction was blocked by the Permissions Policy.');
+            //console.log('Sensor construction was blocked by the Permissions Policy.');
+        } else if (error.name === 'ReferenceError') {
+            alert('Sensor is not supported by the User Agent.');
+            //console.log('Sensor is not supported by the User Agent.');
+        } else {
+            throw error;
+        }
+    }
 //        }
 //    });
 }
