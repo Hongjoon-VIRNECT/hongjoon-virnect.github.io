876c37a19d61b28e0821e0ed35818f08a6f02de0
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-11 11:08:35 +0100

    fixed wrong unit

--- genericSensorApi/sensor.js
+++ genericSensorApi/sensor.js
@@ -1,28 +1,13 @@
-function stopSensors(){
-    gyroscope.stop();
-    accelerometer.stop();
-}
-function startSensors(){
-    //let accelerometer = new LinearAccelerationSensor({ frequency: 1 });
+function startSensors() {
     const accelerometer = new Accelerometer({ frequency: 200 });
     const gyroscope = new Gyroscope({ frequency: 200 });
-    //accelerometer.onerror = (event) => {
-    //// Handle runtime errors.
-    //  if (event.error.name === 'NotAllowedError') {
-    //      alert("Permission to access sensor was denied.");
-    //      //console.log('Permission to access sensor was denied.');
-    //  } else if (event.error.name === 'NotReadableError') {
-    //        alert('Cannot connect to the sensor.');
-    //        //console.log('Cannot connect to the sensor.');
-    //  }
-    //};
     gyroscope.addEventListener("reading", () => {
         let gyroX = gyroscope.x;
         let gyroY = gyroscope.y;
         let gyroZ = gyroscope.z;
-        var gyroXStr = 'X-Pitch(deg): ' + gyroX;
-        var gyroYStr = 'Y-Roll(deg): ' + gyroY;
-        var gyroZStr = 'Z-Yaw(deg): ' + gyroZ;
+        var gyroXStr = 'X-Pitch(rad/s): ' + gyroX;
+        var gyroYStr = 'Y-Roll(rad/s): ' + gyroY;
+        var gyroZStr = 'Z-Yaw(rad/s): ' + gyroZ;
         document.getElementById("gyro_x_result").innerHTML = gyroXStr;
         document.getElementById("gyro_y_result").innerHTML = gyroYStr;
         document.getElementById("gyro_z_result").innerHTML = gyroZStr;
