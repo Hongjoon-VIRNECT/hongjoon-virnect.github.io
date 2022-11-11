e4684aa4d23f28140525060a2dc21574fa1e77e7
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-11 21:54:13 +0100

    error fix

--- genericSensorApi/sensor.js
+++ genericSensorApi/sensor.js
@@ -1,6 +1,7 @@
 function startSensors() {
-    let accIntNew = 0;
+    //let accIntNew = 0;
     let accIntOld = 0;
+    let accIntDif = 0;
     if('Gyroscope' in window) {
         let gyro = new Gyroscope({ frequency: 1 });
         gyro.addEventListener("reading", () => {
@@ -22,9 +23,7 @@ function startSensors() {
     if('Accelerometer' in window) {
         let accel = new Accelerometer({ frequency: 1 });
         accel.addEventListener("reading", () => {
-            //accIntNew = performance.now() - accIntOld;
-            //accIntOld = accIntNew;
-            //accIntNew = performance.now() - accIntNew;
+            accIntDif = performance.now() - accIntOld;
             
             let accX  = accel.x;
             let accY  = accel.y;
@@ -37,14 +36,15 @@ function startSensors() {
             var accYStr   = 'Acc Y(m/s^2): ' + accY;
             var accZStr   = 'Acc Z(m/s^2): ' + accZ;
             var accTSStr  = 'timestamp: '    + accTS;
-            //var accIntStr = 'Interal[ms]: '  + accIntNew;
-            var accIntStr = 'Interal[ms]: '  + performance.now();
+            var accIntStr = 'Interal[ms]: '  + accIntDif;
+
             document.getElementById("acc_x_result").innerHTML  = accXStr;
             document.getElementById("acc_y_result").innerHTML  = accYStr;
             document.getElementById("acc_z_result").innerHTML  = accZStr;
             document.getElementById("acc_timestamp").innerHTML = accTSStr;
             //if (accNew % 1000) < 1) 
             document.getElementById("acc_interval").innerHTML  = accIntStr;
+            accIntOld = performance.now()
         });
         accel.start();
     } else {
