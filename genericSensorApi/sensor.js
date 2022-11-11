907136f72df5e744dfcee01baca3a6cff06dfb84
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-11 12:48:25 +0100

    to check timestamp

--- genericSensorApi/sensor.js
+++ genericSensorApi/sensor.js
@@ -28,10 +28,11 @@ function startSensors() {
             var accYStr  = 'Acc Y(m/s^2): ' + accY;
             var accZStr  = 'Acc Z(m/s^2): ' + accZ;
             var accTSStr = 'timestamp: '    + accTS;
-            document.getElementById("acc_x_result").innerHTML  = accXStr;
-            document.getElementById("acc_y_result").innerHTML  = accYStr;
-            document.getElementById("acc_z_result").innerHTML  = accZStr;
-            document.getElementById("acc_timestamp").innerHTML = accTSStr;
+            //document.getElementById("acc_x_result").innerHTML  = accXStr;
+            //document.getElementById("acc_y_result").innerHTML  = accYStr;
+            //document.getElementById("acc_z_result").innerHTML  = accZStr;
+            //document.getElementById("acc_timestamp").innerHTML = accTSStr;
+            document.write (accTSStr + '<br>');
         });
         accel.start();
     } else {
