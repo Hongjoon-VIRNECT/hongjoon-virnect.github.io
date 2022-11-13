a71c37ee9fa3b29f437f57da091a46b51e46a32b
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-13 13:12:43 +0100

    added timestamp

--- genericSensorApi2/sensor.js
+++ genericSensorApi2/sensor.js
@@ -4,15 +4,16 @@ function startSensors() {
         var accX  = accel.x;
         var accY  = accel.y;
         var accZ  = accel.z;
-        var timeStamp = performance.now();
+        //var timeStamp = performance.now();
+        var timeStamp = accel.timeStamp;
         handleMotionEvent(accX, accY, accZ, timeStamp);
         }, true);
     accel.start();
 }
 
 const handleMotionEvent = (accX, accY, accZ, timeStamp) => {
-    var accXStr = 'X(m/s^2): ' + accX;
-    var accYStr = 'Y(m/s^2): ' + accY;
-    var accZStr = 'Z(m/s^2): ' + accZ;
-    document.write(timeStamp + '[ms]' + '<br>' + accXStr + '<br>' + accYStr + '<br>' + accZStr + '<br>');
+    //var accXStr = 'X(m/s^2): ' + accX;
+    //var accYStr = 'Y(m/s^2): ' + accY;
+    //var accZStr = 'Z(m/s^2): ' + accZ;
+    document.write(timeStamp + ',' + accX + ',' + accY + ',' + accZ + '<br>');
 }
