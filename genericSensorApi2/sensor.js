38b9096f9998ad43c684fc167300b700e241756c
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-14 10:22:14 +0100

    fixed frequency to 100hz

--- genericSensorApi2/sensor.js
+++ genericSensorApi2/sensor.js
@@ -1,20 +1,19 @@
 function startSensors() {
-    let accel = new Accelerometer({ frequency: 1000 });
+    let accel = new Accelerometer({ frequency: 100 });
     accel.addEventListener("reading", () => {
         var accX  = accel.x;
         var accY  = accel.y;
         var accZ  = accel.z;
-        //var timeStamp = performance.now();
-        var timeStamp = accel.timeStamp;
+        //performance.now() and sensor.timeStamp or event.timestamp
         var ts = accel.timestamp;
-        handleMotionEvent(accX, accY, accZ, timeStamp, ts);
+        handleMotionEvent(accX, accY, accZ, ts);
         }, true);
     accel.start();
 }
 
-const handleMotionEvent = (accX, accY, accZ, timeStamp, ts) => {
+const handleMotionEvent = (accX, accY, accZ, ts) => {
     //var accXStr = 'X(m/s^2): ' + accX;
     //var accYStr = 'Y(m/s^2): ' + accY;
     //var accZStr = 'Z(m/s^2): ' + accZ;
-    document.write(timeStamp + ',' + ts + ',' + accX + ',' + accY + ',' + accZ + '<br>');
+    document.write(ts + ',' + accX + ',' + accY + ',' + accZ + '<br>');
 }
