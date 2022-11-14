c0d910bf261ea05dce25b6cf51b398dc4b4fc003
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-14 10:25:43 +0100

    added performance now()

--- genericSensorApi2/sensor.js
+++ genericSensorApi2/sensor.js
@@ -5,15 +5,16 @@ function startSensors() {
         var accY  = accel.y;
         var accZ  = accel.z;
         //performance.now() and sensor.timeStamp or event.timestamp
+        var now = performance.now()
         var ts = accel.timestamp;
-        handleMotionEvent(accX, accY, accZ, ts);
+        handleMotionEvent(accX, accY, accZ, now, ts);
         }, true);
     accel.start();
 }
 
-const handleMotionEvent = (accX, accY, accZ, ts) => {
+const handleMotionEvent = (accX, accY, accZ, now, ts) => {
     //var accXStr = 'X(m/s^2): ' + accX;
     //var accYStr = 'Y(m/s^2): ' + accY;
     //var accZStr = 'Z(m/s^2): ' + accZ;
-    document.write(ts + ',' + accX + ',' + accY + ',' + accZ + '<br>');
+    document.write(ts + ',' + now + ',' + accX + ',' + accY + ',' + accZ + '<br>');
 }
