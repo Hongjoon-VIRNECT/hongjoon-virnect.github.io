515a54aa00e580ce74699707258917d9bd44fc40
Hongjoon-VIRNECT <hongjoon.lim@virnect.com>
2022-11-15 10:58:17 +0100

    added 200hz application

---
+++ genericSensorApi200Hz/sensor.js
@@ -0,0 +1,20 @@
+function startSensors() {
+    let lastReadingTimestamp;
+    let accInterval;
+    let accel = new Accelerometer({ frequency: 200 });
+    accel.addEventListener("reading", () => {
+        if (lastReadingTimestamp) {
+            accInterval = Math.round(accel.timestamp - lastReadingTimestamp);
+        }
+        lastReadingTimestamp = accel.timestamp;
+        var accX  = accel.x;
+        var accY  = accel.y;
+        var accZ  = accel.z;
+        handleMotionEvent(accInterval, accX, accY, accZ);
+        }, true);
+    accel.start();
+}
+
+const handleMotionEvent = (accInterval, accX, accY, accZ) => {
+    document.write('Freq:200Hz,' + 'interval(ms):' + accInterval + ',' + accX + ',' + accY + ',' + accZ + '<br>');
+}
